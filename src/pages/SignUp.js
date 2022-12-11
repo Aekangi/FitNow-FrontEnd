c75011746b6f37c47b9e2164eb79523c90db5cee
Aekangi Patel <aekangi91@gmail.com>
2022-12-10 22:19:31 -0500

    signup page form made

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,8 +4,85 @@ import { useNavigate } from 'react-router-dom'
 
 function SignUp() {
   const navigate = useNavigate()
+  const initialFormValues = {
+    name: '',
+    username: '',
+    password: '',
+    email: ''
+  }
 
-  return <div>SignUp</div>
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await RegisterUser({
+      username: formValues.username,
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password
+    })
+    setFormValues({
+      name: '',
+      username: '',
+      email: '',
+      password: ''
+    })
+    navigate('/login')
+  }
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            onChange={handleChange}
+            name="name"
+            type="text"
+            placeholder="John Doe"
+            value={formValues.name}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="usernama">Username</label>
+          <input
+            onChange={handleChange}
+            type="text"
+            name="username"
+            value={formValues.username}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            name="email"
+            type="email"
+            placeholder="example@example.com"
+            value={formValues.email}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            onChange={handleChange}
+            type="password"
+            name="password"
+            value={formValues.password}
+            required
+          />
+        </div>
+        <button>Sign In</button>
+      </form>
+    </div>
+  )
 }
 
 export default SignUp
