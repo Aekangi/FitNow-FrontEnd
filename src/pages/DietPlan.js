33ef20ffaa3f156de195392e2027d171b82c609f
Aekangi Patel <aekangi91@gmail.com>
2022-12-11 02:46:54 -0500

    consolw returns all of the diet plans

--- src/pages/DietPlan.js
+++ src/pages/DietPlan.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import DietPlanCard from '../components/DietPlanCard'
+import { Link } from 'react-router-dom'
+import { BASE_URL } from '../services/Api'
 
-function DietPlan() {
-  return <div>DietPlan</div>
+const DietPlan = () => {
+  const [dietPlans, setDietPlans] = useState([])
+
+  const getDietPlan = async () => {
+    const response = await axios.get(`${BASE_URL}/dietplans`)
+    setDietPlans(response.data)
+  }
+
+  useEffect(() => {
+    getDietPlan()
+  }, [])
+
+  return (
+    <div>
+      <h1>Diet Plans</h1>
+    </div>
+  )
 }
 
 export default DietPlan
